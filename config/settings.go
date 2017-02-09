d26dbd9c15c404285787b43d6302899972380c9e
Qiang Li <qiang4.li@ge.com>
2017-02-08 18:18:31 -0800

    support json as env value

--- config/settings.go
+++ config/settings.go
@@ -5,31 +5,41 @@ import (
 	"github.com/cloudfoundry-community/go-cfenv"
 	"strconv"
 	"sync"
+	"encoding/json"
+	"fmt"
 )
 
 type Settings struct {
 	Env   *cfenv.App
 
-	cache map[string]string //cached env and uris
+	cache map[string]interface{} //cached env and uris
 
 	sync.Mutex
 }
 
-func (s Settings) getEnv(name string) string {
+func (s Settings) String() string {
+	return fmt.Sprintf("%s", s.cache)
+}
+
+func (s Settings) getEnv(name string) interface{} {
 	s.Lock()
 	defer s.Unlock()
 
 	key := "env_" + name
 
-	var val string
-	var present bool
-	if val, present = s.cache[key]; present {
-		return val
+	var t interface{}
+	var ok bool
+	if t, ok = s.cache[key]; ok {
+		return t
 	}
-	val = os.Getenv(name)
-	s.cache[key] = val
 
-	return val
+	v := os.Getenv(name)
+	if err := json.Unmarshal([]byte(v), &t); err != nil {
+		s.cache[key] = v
+		return v
+	}
+	s.cache[key] = t
+	return t
 }
 
 func (s Settings) getUriByName(name string) string {
@@ -38,22 +48,22 @@ func (s Settings) getUriByName(name string) string {
 
 	key := "name_" + name
 
-	var val string
+	var t interface{}
 	var ok bool
-	if val, ok = s.cache[key]; ok {
-		return val
+	if t, ok = s.cache[key]; ok {
+		return t.(string)
 	}
 
 	svc, err := s.Env.Services.WithName(name)
 	if err != nil {
-		val = ""
+		t = ""
 	} else {
 
-		val = svc.Credentials["uri"].(string)
+		t = svc.Credentials["uri"].(string)
 	}
-	s.cache[key] = val
+	s.cache[key] = t
 
-	return val
+	return t.(string)
 }
 
 func (s Settings) getUriByLabel(label string) string {
@@ -62,22 +72,22 @@ func (s Settings) getUriByLabel(label string) string {
 
 	key := "label_" + label
 
-	var val string
+	var t interface{}
 	var ok bool
-	if val, ok = s.cache[key]; ok {
-		return val
+	if t, ok = s.cache[key]; ok {
+		return t.(string)
 	}
 
 	svc, err := s.Env.Services.WithLabel(label)
 	if err != nil {
-		val = ""
+		t = ""
 	} else {
 
-		val = svc[0].Credentials["uri"].(string)
+		t = svc[0].Credentials["uri"].(string)
 	}
-	s.cache[key] = val
+	s.cache[key] = t
 
-	return val
+	return t.(string)
 }
 
 func (s Settings) getUri(labels []string, name ...string) string {
@@ -105,19 +115,75 @@ func (s Settings) RabbitmqUri(name ...string) string {
 	return s.getUri(labels, name ...)
 }
 
-func (s Settings) GetEnv(key string) string {
-	return s.getEnv(key)
+func (s Settings) ServiceUri(name ...string) string {
+	labels := name
+	return s.getUri(labels, name ...)
 }
 
-func (s Settings) GetIntEnv(key string, def int) int {
+func (s Settings) GetEnv(key string, name ...string) interface{} {
 	v := s.getEnv(key)
-	if v != "" {
-		i, err := strconv.Atoi(v)
-		if err == nil {
-			return i
+	if len(name) == 0 {
+		return v
+	}
+
+	return traverse(name, v.(map[string]interface{}))
+}
+
+func (s Settings) GetStringEnv(key string, name ...string) string {
+	t := s.GetEnv(key, name...)
+
+	switch t.(type) {
+	case map[string]interface{}:
+	case []interface{}:
+	case string:
+		return t.(string)
+	case nil:
+		return ""
+	case int:
+	case float64:
+	case bool:
+	default:
+	}
+
+	return fmt.Sprintf("%v", t)
+}
+
+func (s Settings) GetBoolEnv(key string, name ...string) bool {
+	t := s.GetEnv(key, name...)
+
+	b, err := strconv.ParseBool(fmt.Sprintf("%v", t))
+	if err == nil {
+		return b
+	}
+	return false
+}
+
+func (s Settings) GetIntEnv(key string, name ...string) int {
+	t := s.GetEnv(key, name...)
+
+	i, err := strconv.Atoi(fmt.Sprintf("%v", t))
+	if err == nil {
+		return i
+	}
+
+	return 0
+}
+
+func traverse(name []string, m map[string]interface{}) interface{} {
+	switch len(name) {
+	case 0:
+		return m
+	case 1:
+		return m[name[0]]
+	default:
+		v := m[name[0]]
+		switch v.(type) {
+		case map[string]interface{}:
+			return traverse(name[1:], v.(map[string]interface{}))
 		}
 	}
-	return def
+
+	return nil
 }
 
 func NewSettings() Settings {
@@ -126,7 +192,7 @@ func NewSettings() Settings {
 
 	s = Settings{
 		Env: env,
-		cache: make(map[string]string),
+		cache: make(map[string]interface{}),
 	}
 	return s
 }
